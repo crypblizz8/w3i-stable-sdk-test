03474451206b48681bb3b2f871c96cf245a27c7b
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2024-01-31 15:10:44 +0530

    fix: frontend enhancements and Messages loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -127,7 +127,7 @@ export default function Home() {
               </div>
               <hr />
             </div>
-            <Messages />
+            {isSubscribed && <Messages />}
           </div>
         )}
       </main>
